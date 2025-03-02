d5014a87380b0313ad24f80e5fc23499c8d0fcd0
raj-khanna1 <raj-khanna1>
2025-03-02 22:47:42 +0530

    version1

---
+++ script.js
@@ -0,0 +1,57 @@
+// function which will run so that front end fetch the required data to show
+
+async function process(){
+    let afterHeading1=document.getElementById('afterHeading1');
+    let afterHeading2=document.getElementById('afterHeading2');
+    let afterHeading3=document.getElementById('afterHeading3');
+    let data= await fetch('https://api-test-server-1.onrender.com/api/websiteData');
+    data=await data.json();
+    data.forEach(obj => {
+        let div=document.createElement('div');
+        afterHeading1.append(div);
+
+        div.className='divis';
+        div.style.display='flex';
+        div.style.justifyContent='space-between';
+        let childDiv1=document.createElement('div');
+        childDiv1.className='divis1';
+        childDiv1.innerHTML=  `<h9>${obj.method_name}</h9>`;
+        childDiv1.style.background='grey';
+        childDiv1.style.color='white';
+        // childDiv1.style.border = '1px solid black';
+        let childDiv2=document.createElement('div');
+        childDiv2.className='divis2';
+        childDiv2.innerHTML=  `<h8>${obj.method_description}</h8>`;
+        childDiv1.style.color='black';
+        childDiv2.style.background='white';
+
+        div.append(childDiv1);
+        div.append(childDiv2);
+
+
+        // adding the click event listner which will write the frontend data in 
+        // other two clms
+        div.addEventListener('click',()=>{
+            // let data=JSON.
+            let damData=(obj.response_body[0]);
+            console.log(damData);
+            
+            afterHeading3.innerHTML = `<pre style="background: #87CEEB; padding: 10px; border-radius: 5px;">${JSON.stringify(damData, null, 2)}</pre>`;
+            let reqUrl=document.getElementById('reqUrl2');
+            reqUrl.innerHTML = `<pre style="background: #f4f4f4; padding: 10px; border-radius: 5px;">${obj.response_code}</pre>`;
+        
+        });
+        div.addEventListener('click',()=>{
+            // let data=JSON.
+            let damData=(obj.request_body);
+            console.log(damData);
+            afterHeading2.innerHTML = `<pre style="background: #f4f4f4; padding: 10px; border-radius: 5px;">${JSON.stringify(damData, null, 2)}</pre>`;
+            let reqUrl=document.getElementById('reqUrl');
+            reqUrl.innerHTML = `<pre style="background: #f4f4f4; padding: 10px; border-radius: 5px;">${obj.request_path}</pre>`;
+
+        });
+
+    });
+
+}
+process();
